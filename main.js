df7ad3e991afcde2a7ad24ee2d512a4e8e04675e
Thorsten Muelhoefer <muethos76@gmail.com>
2018-05-16 12:22:31 +0200

    Rename to Brother

--- main.js
+++ main.js
@@ -1,19 +1,19 @@
 /**
  *
- * template adapter
+ * brother_hl-4150 adapter
  *
  *
  *  file io-package.json comments:
  *
  *  {
  *      "common": {
- *          "name":         "template",                  // name has to be set and has to be equal to adapters folder name and main file name excluding extension
+ *          "name":         "brother_hl-4150",                  // name has to be set and has to be equal to adapters folder name and main file name excluding extension
  *          "version":      "0.0.0",                    // use "Semantic Versioning"! see http://semver.org/
- *          "title":        "Node.js template Adapter",  // Adapter title shown in User Interfaces
+ *          "title":        "Node.js brother_hl-4150 Adapter",  // Adapter title shown in User Interfaces
  *          "authors":  [                               // Array of authord
- *              "name <mail@template.com>"
+ *              "name <mail@brother_hl-4150.com>"
  *          ]
- *          "desc":         "template adapter",          // Adapter description shown in User Interfaces. Can be a language object {de:"...",ru:"..."} or a string
+ *          "desc":         "brother_hl-4150 adapter",          // Adapter description shown in User Interfaces. Can be a language object {de:"...",ru:"..."} or a string
  *          "platform":     "Javascript/Node.js",       // possible values "javascript", "javascript/Node.js" - more coming
  *          "mode":         "daemon",                   // possible values "daemon", "schedule", "subscribe"
  *          "materialize":  true,                       // support of admin3
@@ -38,8 +38,8 @@ var utils =    require(__dirname + '/lib/utils'); // Get common adapter utils
 
 // you have to call the adapter function and pass a options object
 // name has to be set and has to be equal to adapters folder name and main file name excluding extension
-// adapter will be restarted automatically every time as the configuration changed, e.g system.adapter.template.0
-var adapter = new utils.Adapter('template');
+// adapter will be restarted automatically every time as the configuration changed, e.g system.adapter.brother_hl-4150.0
+var adapter = new utils.Adapter('brother_hl-4150');
 
 // is called when adapter shuts down - callback has to be called under any circumstances!
 adapter.on('unload', function (callback) {
@@ -100,7 +100,7 @@ function main() {
      *
      *      For every state in the system there has to be also an object of type state
      *
-     *      Here a simple template for a boolean variable named "testVariable"
+     *      Here a simple brother_hl-4150 for a boolean variable named "testVariable"
      *
      *      Because every adapter instance uses its own unique namespace variable names can't collide with other adapters variables
      *
@@ -116,7 +116,7 @@ function main() {
         native: {}
     });
 
-    // in this template all states changes inside the adapters namespace are subscribed
+    // in this brother_hl-4150 all states changes inside the adapters namespace are subscribed
     adapter.subscribeStates('*');
 
 
